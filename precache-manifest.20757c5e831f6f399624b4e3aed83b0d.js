617af9b57d18b4d00c23d5d2aa76e140b2a41f14
marianskrzypekk <63640985+marianskrzypekk@users.noreply.github.com>
2020-10-16 10:09:00 +0200

    Add files via upload

---
+++ precache-manifest.20757c5e831f6f399624b4e3aed83b0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5103f78fb313a859cb70b5251c1a1cf9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4307ca2c1ff291d3de1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6d44f8c11febb5b8a642",
+    "url": "/static/js/2.f26339cd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.f26339cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4307ca2c1ff291d3de1",
+    "url": "/static/js/main.15fffacc.chunk.js"
+  },
+  {
+    "revision": "42428212c041f4a4711f",
+    "url": "/static/js/runtime-main.6df2c58c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
